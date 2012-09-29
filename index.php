bc7da44dff26581b05f5637dd093e920345c2614
Laurent Cazalet <lrntczlt@eml.cc>
2012-09-29 17:25:51 +0200

    included documentation

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+/* this code section is for first install only
+
+NOT NEEDED IN A PRODUCTION ENVIRONMENT
+
+*/
+
 require_once('newsfeeds/init.php');
 // config.php exists and is writable
 	if(!is_writable('newsfeeds/config.php')) {
@@ -15,7 +21,11 @@ require_once('newsfeeds/init.php');
 </head>
 <body>
 <h1>ZebraFeeds Demo</h1>
-This is a demo of ZebraFeeds as a personal news aggregator.
+This is a demo of ZebraFeeds as a personal news aggregator. <br/>
+Check out the <a href="documentation/doc.html">documentation</a> to start customizing it for your website.
+<p>
+Other demos: <a href="demo_rssfeed.php">RSS feed</a> - <a href="demo_js.html">Javascript integration</a> - <a href="demo_manual.php">manual control of feeds rendering</a>
+</p>
 <hr/>
 <div style="
 	font-size: 80%;
