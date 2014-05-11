f4ead2fdd97c851ef8f488f3b101b22ed1c6a6da
Laurent Cazalet <lrntczlt@eml.cc>
2014-05-11 23:34:27 +0200

    remove obsolete demo link

--- index.php
+++ index.php
@@ -23,9 +23,6 @@ require_once('newsfeeds/init.php');
 <h1>ZebraFeeds Demo</h1>
 This is a demo of ZebraFeeds as a personal news aggregator. <br/>
 Check out the <a href="documentation/doc.html">documentation</a> to start customizing it for your website.
-<p>
-Other demos: <a href="demo_rssfeed.php">RSS feed</a> - <a href="demo_js.html">Javascript integration</a> - <a href="demo_manual.php">manual control of feeds rendering</a>
-</p>
 <hr/>
 <div style="	vertical-align: top;
 	margin-left: 15px;
