58c9acb76bd3336e748365bd134d5d3898b6168f
Laurent Cazalet <lrntczlt@eml.cc>
2015-04-12 00:01:08 +0200

    new setting

--- globals.php
+++ globals.php
@@ -13,7 +13,7 @@ define('ZF_SHOWCREDITS', 'no');
 // ideally, this should be defined in the template
 //define('ZF_ISNEW_STRING', '<img src="'.ZF_URL.'res/img/new.png" border="0" title="is new since last visit" alt="New"/>');
 define('ZF_ISNEW_STRING', 'newclass');
-
+define('ZF_DEFAULT_ADMIN_VIEW', 'subscriptions');
 
 /*--------------------------*/
 /* bit debug values for ZF_DEBUG */
